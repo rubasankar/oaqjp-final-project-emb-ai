b661f799b722c3a4ea200c30cf95f34457263314
rubasankar <rubasankar2002@gmail.com>
2024-03-03 12:01:54 -0500

    Deployment

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,48 @@
+"""
+Emotion Detection Module
+"""
+import json
+import requests
+
+def emotion_detector(text_to_analyse):
+    """
+    emotion detection function
+    input : str
+    output : Dict
+    """
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url,headers=header,json=myobj,timeout=120)
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        return emotion_predictor(formatted_response)
+    elif response.status_code == 400:
+        formatted_response = {
+                            'anger': None,
+                            'disgust': None, 
+                            'fear': None, 
+                            'joy': None, 
+                            'sadness': None, 
+                            'dominant_emotion': None}
+        return formatted_response
+
+def emotion_predictor(detected_text):
+    if detected_text['emotionPredictions'] is not None:
+        emotions = detected_text['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        dominant_emotion = max(emotions, key=emotions.get)
+        formatted_emotions_dict = {
+                                'anger': anger,
+                                'disgust': disgust,
+                                'fear': fear,
+                                'joy': joy,
+                                'sadness': sadness,
+                                'dominant_emotion': dominant_emotion
+                                }
+        return formatted_emotions_dict
+    
---
+++ server.py
@@ -0,0 +1,37 @@
+"""
+Executing this file initiates the application of emotion detection to be 
+executed over the Flask channel and deployed on localhost:5000.
+"""
+from flask import Flask,request,render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route('/emotionDetector')
+def send_detector():
+    """
+        This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detection()
+        function. The output returned shows the overall emotions and its 
+        score and dominant emotion based on scores for the provided text.
+    """
+    text_to_detect = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_detect)
+    if response['dominant_emotion'] is None:
+        return "Invallid text! Please try again!."
+    return (f"For the given statement, the system response is 'anger': "
+        f"{response['anger']}, 'disgust': {response['disgust']}, 'fear': "
+        f"{response['fear']}, 'joy': {response['joy']} and 'sadness': "
+        f"{response['sadness']}. The dominant emotion is "
+        f"{response['dominant_emotion']}.")    
+
+@app.route('/')
+def render_index_page():
+    ''' 
+    This function initiates the rendering of the main application
+    page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000)
